23856430021024d9ed9df2313c17899da4136fbd
Red <it_red@sina.com>
2015-09-02 14:14:36 +0800

    Date日期工具类
    时间戳和时长转换。

--- src/com/red/utilsbox/DateUtil.java
+++ src/com/red/utilsbox/DateUtil.java
@@ -1,6 +1,7 @@
 package com.red.utilsbox;
 
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
@@ -42,9 +43,67 @@ public class DateUtil {
         return sdf.format(date);
     }
 
-    // public static String
+    /**
+     * 获取当前时间戳
+     * @return long型时间戳
+     */
+    public static long getUnixTime() {
+        long unixtime = System.currentTimeMillis() / 1000;
+        return unixtime;
+    }
+
+    /**
+     * 将时间戳转换为指定格式的日期字符串
+     * @param unixtime 10位时间戳<code> eg:1441165597</code>
+     * @param pattern 日期格式参数
+     * @return 格式化后的日期字符串
+     */
+    public static String getDateFromUnixtime(long unixtime, String pattern) {
+        Date date = new Date(unixtime * 1000);
+        SimpleDateFormat sdf = new SimpleDateFormat(pattern);
+        return sdf.format(date);
+    }
+
+    /**
+     * 时长转换
+     * @param second 秒<code>.eg:125635</code>
+     * @return 包含时分秒的字符串<code>.eg:34小时53分钟55秒</code>
+     */
+    public static String changeSecond(int second) {
+        StringBuffer sb = new StringBuffer();
+        if (second > 3600) {
+            sb.append(second / 3600 + "小时");
+        }
+        second = second % 3600;
+        sb.append(second / 60 + "分钟" + second % 60 + "秒");
+        return sb.toString();
+    }
+    
+    /**
+     * 获取两日期之间的天数
+     * @param startdate
+     * @param enddate
+     * @return
+     */
+    public static int getBetweenDates(String begindate,String enddate){
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+        Date bDate = null;
+        Date eDate = null;
+        int day = 0;
+        try {
+            bDate = sdf.parse(begindate);
+            eDate = sdf.parse(enddate);
+            long result = eDate.getTime() - bDate.getTime();
+            day = (int) (result / 86400000L);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return day + 1;
+    }
 
     public static void main(String[] args) {
-        System.out.println(getFormateDate(new Date(), "yyyy.MM.dd HH:mm:ss"));
+        System.out.println(getUnixTime());
+        System.out.println(changeSecond(125635));
+        System.out.println(getDateFromUnixtime(1441165017, "yyyy.MM.dd HH:mm:ss"));
     }
 }
