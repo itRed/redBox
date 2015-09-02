e5f7bf084926914d0eb9070eee05f89454876646
Red <it_red@sina.com>
2015-09-02 14:16:07 +0800

    Calendar日期工具类
    常用月，天的基本计算。

--- src/com/red/utilsbox/CalendarUtil.java
+++ src/com/red/utilsbox/CalendarUtil.java
@@ -11,10 +11,12 @@ import java.util.Date;
 public class CalendarUtil {
 
     /**
-     * 获取当前月的最后一天的日期
-     * @return 本月最后一天的日期
-     * @output Sat Aug 01 11:05:11 GMT+08:00 2015 
-     * <p>需要根据实际情况转换</p>
+     * 获取当前月的第一天的日期
+     * @return 本月第一天的日期
+     * @output Sat Aug 01 11:05:11 GMT+08:00 2015
+     *         <p>
+     *         需要根据实际情况转换
+     *         </p>
      */
     public static Date getFirstDayOfMonth() {
         Calendar calendar = Calendar.getInstance();// 获取当前日期
@@ -26,8 +28,10 @@ public class CalendarUtil {
     /**
      * 获取上个月的第一天
      * @return 上月第一天的日期
-     * @output Sat Aug 01 11:05:11 GMT+08:00 2015 
-     * <p>需要根据实际情况转换</p>
+     * @output Sat Aug 01 11:05:11 GMT+08:00 2015
+     *         <p>
+     *         需要根据实际情况转换
+     *         </p>
      */
     public static Date getFirstDayOfLastMonth() {
         Calendar calendar = Calendar.getInstance();// 获取当前日期
@@ -37,10 +41,27 @@ public class CalendarUtil {
     }
 
     /**
+     * 获取上个月的最后一天
+     * @return 上月最后一天的日期
+     * @output Sat Aug 01 11:05:11 GMT+08:00 2015
+     *         <p>
+     *         需要根据实际情况转换
+     *         </p>
+     */
+    public static Date getLastDayOfLastMonth() {
+        Calendar calendar = Calendar.getInstance();// 获取当前日期
+        calendar.add(Calendar.MONTH, -1);
+        calendar.set(Calendar.DAY_OF_MONTH, calendar.getActualMaximum(Calendar.DAY_OF_MONTH));
+        return calendar.getTime();
+    }
+
+    /**
      * 获取当前月份的最后一天的日期
      * @return 当前月份最后一天的日期
-     * @output Sat Aug 01 11:05:11 GMT+08:00 2015 
-     * <p>需要根据实际情况转换</p>
+     * @output Sat Aug 01 11:05:11 GMT+08:00 2015
+     *         <p>
+     *         需要根据实际情况转换
+     *         </p>
      */
     public static Date getLastDayOfMonth() {
         Calendar calendar = Calendar.getInstance();// 获取当前日期
@@ -48,8 +69,28 @@ public class CalendarUtil {
         return calendar.getTime();
     }
 
+    /**
+     * 获取本月一共多少天
+     * @return 本月天数
+     */
+    public static int getDaysOfMonth() {
+        Calendar calendar = Calendar.getInstance();
+        return calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
+    }
+
+    /**
+     * 获取指定日期月份的天数
+     * @param date 指定日期Date
+     * @return 天数
+     */
+    public static int getDaysOfDate(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        return calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
+    }
+
     public static void main(String[] args) {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy:MM:dd");
-        System.out.println(getFirstDayOfLastMonth());
+        System.out.println(getDaysOfDate(new Date()));
     }
 }
