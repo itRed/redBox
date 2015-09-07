2c7bf20d493aa0cb9f0e6cb52a3fb4c04e9b9e59
Red <it_red@sina.com>
2015-09-07 11:50:23 +0800

    排序工具类
    添加选择排序算法和二分法查找算法。

--- src/com/red/utilsbox/SortUtil.java
+++ src/com/red/utilsbox/SortUtil.java
@@ -23,6 +23,69 @@ public class SortUtil {
         }
     }
 
+    /**
+     * 效率高一点的选择排序
+     * @param a
+     */
+    private static void selectionSort2(int[] a) {
+        int k, temp;
+        for (int i = 0; i < a.length; i++) {
+            k = i;
+            for (int j = k + 1; j < a.length; j++) {
+                if (a[j] < a[k]) {
+                    k = j;
+                }
+                if (k != i) {
+                    temp = a[i];
+                    a[i] = a[k];
+                    a[k] = temp;
+                }
+            }
+        }
+    }
+
+    /**
+     * 效率不高的选择排序
+     * @param a
+     */
+    private static void selectionSort(int[] a) {
+        for (int i = 0; i < a.length; i++) {
+            for (int j = i + 1; j < a.length; j++) {
+                if (a[j] < a[i]) {
+                    int temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
+            }
+        }
+    }
+
+    /**
+     * 二分法查找算法
+     * @param a
+     * @param i
+     * @return
+     */
+    private static int arraySearch(int[] a, int i) {
+        if (a.length == 0) return -1;
+
+        int startPos = 0;
+        int endPos = a.length - 1;
+        int m = (startPos + endPos) / 2;
+        System.out.println(a[m]);
+        while (startPos <= endPos) {
+            if (a[m] == i) return m;
+            if (a[m] < i) {
+                startPos = m + 1;
+            }
+            if (a[m] > i) {
+                endPos = m - 1;
+            }
+            m = (startPos + endPos) / 2;
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
         // TODO Auto-generated method stub
 
