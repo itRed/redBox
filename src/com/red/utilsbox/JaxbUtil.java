d8a678fdf3146e24c5f2423aa4ca13adc38bf684
Red <it_red@sina.com>
2017-03-07 21:31:45 +0800

    XML工具类
    新加XML处理的工具类，未添加相关测试方法。

---
+++ src/com/red/utilsbox/JaxbUtil.java
@@ -0,0 +1,66 @@
+package com.red.utilsbox;
+
+import java.io.StringReader;
+import java.io.StringWriter;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+
+public class JaxbUtil {
+	/**
+	 * JavaBean转换成xml 默认编码UTF-8
+	 * 
+	 * @param obj
+	 * @param writer
+	 * @return
+	 */
+	public static String convertToXml(Object obj) {
+		return convertToXml(obj, "UTF-8");
+	}
+
+	/**
+	 * JavaBean转换成xml
+	 * 
+	 * @param obj
+	 * @param encoding
+	 * @return
+	 */
+	public static String convertToXml(Object obj, String encoding) {
+		String xmlStr = null;
+		try {
+			JAXBContext context = JAXBContext.newInstance(obj.getClass());
+			Marshaller marshaller = context.createMarshaller();
+			marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+			marshaller.setProperty(Marshaller.JAXB_ENCODING, encoding);
+			marshaller.setProperty(Marshaller.JAXB_FRAGMENT, true);
+
+			StringWriter writer = new StringWriter();
+			marshaller.marshal(obj, writer);
+			xmlStr = writer.toString();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return xmlStr;
+	}
+
+	/**
+	 * xml转换成JavaBean
+	 * 
+	 * @param xml
+	 * @param c
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	public static <T> T converyToJavaBean(String xml, Class<T> c) {
+		T t = null;
+		try {
+			JAXBContext context = JAXBContext.newInstance(c);
+			Unmarshaller unmarshaller = context.createUnmarshaller();
+			t = (T) unmarshaller.unmarshal(new StringReader(xml));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return t;
+	}
+}
